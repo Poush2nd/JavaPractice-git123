d09f0f79c7a52ffecbce02f0a7d2938987095176
Poush2nd <prajanitim@gmail.com>
2023-07-17 22:56:08 -0500

    day1'

---
+++ Day1.java
@@ -0,0 +1,22 @@
+
+public class Day1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("hello");	
+	
+	
+    int x=5;
+    int y=25;
+    
+    System.out.println(x+y);
+    System.out.println(x-y);
+    System.out.println(x*y);
+    System.out.println(x/y);
+    System.out.println(x%y);
+    
+		
+	}
+
+}
